e6043890b31867e98fdfaa58cf7462be191cdd87
justhenrique <washington.sousa01@fatec.sp.gov.br>
2022-04-05 08:41:05 -0300

    feat(UserList): first things

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Register from '@/components/Register'
+import Vue from "vue";
+import Router from "vue-router";
+import Register from "@/components/Register";
+import UserList from "@/components/UserList";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'Register',
-      component: Register
-    }
-  ]
-})
+      path: "/",
+      name: "Register",
+      component: Register,
+    },
+    {
+      path: "/user-list",
+      name: "User List",
+      component: UserList,
+    },
+  ],
+});
